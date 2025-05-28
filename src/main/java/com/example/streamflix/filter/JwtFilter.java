725cda1e6a4de312a056982adc6bf285f7718841
Víctor Fernández García <victor.fernandez@agilecontent.com>
2025-05-28 12:05:20 +0200

    JWT security implementation

--- src/main/java/com/example/streamflix/filter/JwtFilter.java
+++ src/main/java/com/example/streamflix/filter/JwtFilter.java
@@ -1,6 +1,6 @@
 package com.example.streamflix.filter;
 
-import com.example.streamflix.util.JwtUtil;
+import com.example.streamflix.utils.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
